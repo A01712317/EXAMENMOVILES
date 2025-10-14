9bed81cedb87c065b03ac48f75b8e637ac227058
A01712317 <A01712317@tec.mx>
2025-10-14 11:09:03 -0600

    primer commit

---
+++ app/src/main/java/com/app/examenapp/presentation/screens/home/HomeUiState.kt
@@ -0,0 +1,7 @@
+package com.app.examenapp.presentation.screens.home
+
+data class HomeUiState(
+   // val pokemonList: List<Pokemon> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null,
+)
